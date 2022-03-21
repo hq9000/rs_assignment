19b9ac4a505311b32e0f4d55acc966f2c0b2fa99
Sergey Grechin <grechin.sergey@gmail.com>
2022-03-21 09:42:13 +0300

    committing forgotten fixture gen class

---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Roadsurfer\DataFixtures;
+
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+use Roadsurfer\DependencyInjection\CounterGridServiceAware;
+use Roadsurfer\Entity\EquipmentType;
+use Roadsurfer\Entity\Station;
+
+class AppFixtures extends Fixture
+{
+    use CounterGridServiceAware;
+
+    public function load(ObjectManager $manager): void
+    {
+        $chair = new EquipmentType();
+        $chair->setName("Chair");
+        $manager->persist($chair);
+
+        $toilet = new EquipmentType();
+        $toilet->setName("Toilet");
+        $manager->persist($toilet);
+
+        $berlin = new Station();
+        $berlin->setName("Berlin");
+        $manager->persist($berlin);
+
+        $manager->flush();
+    }
+}
